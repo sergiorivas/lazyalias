8e8e090fa167ba8cb4292328da720dd963347311
Sergio Rivas <sergiorivas@gmail.com>
2025-08-10 11:26:34 -0700

    release: v0.1.16

--- Formula/lazyalias.rb
+++ Formula/lazyalias.rb
@@ -1,19 +1,19 @@
 class Lazyalias < Formula
   desc "Interactive CLI to manage frequent commands with YAML config and clipboard support"
   homepage "https://github.com/sergiorivas/lazyalias"
-  version "v0.1.15"
+  version "0.1.16"
 
   on_macos do
     on_arm do
-      url "https://github.com/sergiorivas/lazyalias/releases/download/vv0.1.15/lazyalias-darwin-arm64"
-      sha256 "1a401bc09084ef281f93acfc83822ab623d9faa82dfa95d15c2e253942a9e5fa"
+      url "https://github.com/sergiorivas/lazyalias/releases/download/v0.1.16/lazyalias-darwin-arm64"
+      sha256 "4fdf0e6ff370517b2d57f861dd6e76dc02c12a99fa7d6249c3b5872e10ccdd2f"
       def install
         bin.install "lazyalias-darwin-arm64" => "lazyalias"
       end
     end
     on_intel do
-      url "https://github.com/sergiorivas/lazyalias/releases/download/vv0.1.15/lazyalias-darwin-amd64"
-      sha256 "79732c3fe51a92c751e02e356f2938ad20efe310a9addadeb89d975af5947ce9"
+      url "https://github.com/sergiorivas/lazyalias/releases/download/v0.1.16/lazyalias-darwin-amd64"
+      sha256 "937c19f5bf2647e72d30b1733d6b040e97ef4ef07d43d0e3d8bd1e9a2272b19a"
       def install
         bin.install "lazyalias-darwin-amd64" => "lazyalias"
       end
@@ -22,15 +22,15 @@ class Lazyalias < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/sergiorivas/lazyalias/releases/download/vv0.1.15/lazyalias-linux-arm64"
-      sha256 "f634ab479813a9432496864c71c099cb2bb964c8e898b0bd0a4b9956fc2372da"
+      url "https://github.com/sergiorivas/lazyalias/releases/download/v0.1.16/lazyalias-linux-arm64"
+      sha256 "e339a6ce9cedef17274c62c2cb34c901431114a531f6f34ac7d07042fd44c9c1"
       def install
         bin.install "lazyalias-linux-arm64" => "lazyalias"
       end
     end
     on_intel do
-      url "https://github.com/sergiorivas/lazyalias/releases/download/vv0.1.15/lazyalias-linux-amd64"
-      sha256 "a2689b6dfe2667b09b9d9d11b89196300cf183d9aec5a64f9f8f3a6bf9925b57"
+      url "https://github.com/sergiorivas/lazyalias/releases/download/v0.1.16/lazyalias-linux-amd64"
+      sha256 "490e7336b0a02ee8b7a35c9646ce9c191e50d2659e430b5ca6a6295b92f82075"
       def install
         bin.install "lazyalias-linux-amd64" => "lazyalias"
       end
